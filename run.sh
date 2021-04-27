c790d60a1e1ebb387797cd342cca02f9bd5d7eb4
Francisco de Melo <franciscomelojr@gmail.com>
2021-04-27 13:35:49 -0400

    instructions

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
+#   36  2021-04-27 13:14:30 touch run.sh
+#   37  2021-04-27 13:14:33 sublime run.sh 
+#   38  2021-04-27 13:15:12 ls
+#   39  2021-04-27 13:15:19 chmod +x run.sh 
+#   40  2021-04-27 13:15:23 ./run.sh 
+#   64  2021-04-27 13:20:20 ls
+#   65  2021-04-27 13:20:23 git status
+#   66  2021-04-27 13:20:31 git add report.html
+#   67  2021-04-27 13:20:34 git add run.sh
+#   68  2021-04-27 13:20:41 git commit -m "bash script testing"
+#   69  2021-04-27 13:20:45 git push 
+#   70  2021-04-27 13:21:19 clear
+#   71  2021-04-27 13:31:00 ./run.sh 
+#   72  2021-04-27 13:32:55 clear
+#   73  2021-04-27 13:32:55 ls
+#   74  2021-04-27 13:32:57 ./run.sh 
+
 echo "executing script"
 google-chrome report.html
+#opera report.html
